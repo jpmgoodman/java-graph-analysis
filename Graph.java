39e729789ab12af725446348eb8f64d3b51ecf26
Jesse Goodman <jpmg@princeton.edu>
2015-07-28 00:07:24 -0700

    Initial class

---
+++ Graph.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Graph {
+    int[][] adjMatrix;
+
+    public Graph(int[][] adjMatrix) {
+
+    }
+
+    public static void main(String[] args) throws java.io.IOException {
+        Scanner stdin = new Scanner(System.in);
+        // use lists?
+    }
+}
