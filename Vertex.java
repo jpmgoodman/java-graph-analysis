393f7453aeb4e71c66f2fe914f13720a11405754
Jesse Goodman <jpmg@princeton.edu>
2015-08-01 19:47:17 -0700

    Added Vertex class

--- Vertex.java
+++ Vertex.java
@@ -1,13 +1,16 @@
+import java.util.List;
+
 public class Vertex {
 
     private List<Edge> edges;
+    private int degree;
 
-    public Vertex() {
-
+    public Vertex(List<Edge> edges) {
+        this.edges = edges;
+        this.degree = edges.size();
     }
 
-
-
-
-
+    public int degree() {
+        return this.degree;
+    }
 }
